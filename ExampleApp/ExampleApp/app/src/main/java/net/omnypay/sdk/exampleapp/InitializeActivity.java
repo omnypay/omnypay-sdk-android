99f88143c7fd0478a077120f157957eb5c66d99d
MikiP26 <miki.padhiary@talentica.com>
2016-12-16 14:53:40 +0530

    Added Copyright License

--- ExampleApp/ExampleApp/app/src/main/java/net/omnypay/sdk/exampleapp/InitializeActivity.java
+++ ExampleApp/ExampleApp/app/src/main/java/net/omnypay/sdk/exampleapp/InitializeActivity.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2016 OmnyPay Inc.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.omnypay.sdk.exampleapp;
 
 import android.app.ProgressDialog;
@@ -35,12 +50,12 @@ public class InitializeActivity extends AppCompatActivity implements View.OnClic
     /**
      * REPLACE MERCHANTID WITH YOUR MERCHANT ID
      */
-    private String merchantId = null ;
+    private String merchantId = null;
 
     /**
      * YOUR USERNAME
      */
-    private String username = null ;
+    private String username = null;
 
     /**
      * YOUR PASSWORD
@@ -82,7 +97,7 @@ public class InitializeActivity extends AppCompatActivity implements View.OnClic
 
                     @Override
                     public void onFailure(final OmnyPayError omnyPayError) {
-                        if (merchantId == null ) {
+                        if (merchantId == null) {
                             showToast("Merchant ID is not set correctly");
 
                         } else {
