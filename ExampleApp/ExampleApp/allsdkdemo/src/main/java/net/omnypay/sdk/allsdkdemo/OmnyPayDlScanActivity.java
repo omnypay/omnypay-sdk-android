2e2b771b09b178a5f42e954979c640f4de949e36
shaktisinghmoyal <shakti.singh0708@gmail.com>
2017-01-09 12:37:32 +0530

    android permission implemented for camrera

--- ExampleApp/ExampleApp/allsdkdemo/src/main/java/net/omnypay/sdk/allsdkdemo/OmnyPayDlScanActivity.java
+++ ExampleApp/ExampleApp/allsdkdemo/src/main/java/net/omnypay/sdk/allsdkdemo/OmnyPayDlScanActivity.java
@@ -1,7 +1,12 @@
 package net.omnypay.sdk.allsdkdemo;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -17,7 +22,10 @@ import net.omnypay.omnypaydlscan.model.IdentityData;
 public class OmnyPayDlScanActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button scanButton;
+    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 1;
+    private static Snackbar snackbar;
     private TextView firstNameValue, lastNameValue, postalCodeValue;
+    private  String[] PERMISSIONS_CAMERA = {Manifest.permission.CAMERA,};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +40,41 @@ public class OmnyPayDlScanActivity extends AppCompatActivity implements View.OnC
 
     @Override
     public void onClick(View view) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED
+                ) {
+            requestCameraPermissions(scanButton);
+        } else {
+            //TODO some method
+            startDLScan();
+        }
 
+    }
+
+    private  void requestCameraPermissions(View randomView) {
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.CAMERA)) {
+            snackbar = Snackbar.make(randomView, R.string.permission_camera_rationale,
+                    Snackbar.LENGTH_INDEFINITE)
+                    .setAction(R.string.decide, new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            ActivityCompat
+                                    .requestPermissions(OmnyPayDlScanActivity.this,
+                                            PERMISSIONS_CAMERA,
+                                            MY_PERMISSIONS_REQUEST_CAMERA);
+                        }
+                    });
+
+            snackbar.show();
+        } else {
+            ActivityCompat.requestPermissions(OmnyPayDlScanActivity.this, PERMISSIONS_CAMERA,
+                    MY_PERMISSIONS_REQUEST_CAMERA);
+        }
+    }
+
+    private void startDLScan() {
         OmnyPayDLScanner.getInstance().start(
                 OmnyPayDlScanActivity.this, new DriversLicenseScanCallback() {
                     @Override
@@ -42,6 +84,28 @@ public class OmnyPayDlScanActivity extends AppCompatActivity implements View.OnC
                         postalCodeValue.setText(String.valueOf(identityData.getPostCode()));
                     }
                 });
+    }
+
+    @Override
+    public  void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+        if (requestCode == MY_PERMISSIONS_REQUEST_CAMERA) {
+
+
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+
+//                snackbar = Snackbar.make(scanButton, R.string.permission_granted, Snackbar.LENGTH_SHORT);
+//                snackbar.show();
+                startDLScan();
+
+            } else {
+                snackbar = Snackbar.make(scanButton, R.string.permission_not_granted, Snackbar.LENGTH_SHORT);
+                snackbar.show();
+
+            }
+        }
+
     }
 
 }
